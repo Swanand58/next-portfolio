fbc7476560c6e3635128c97f4ed877f7f0a90a72
Swanand58 <swanandkhonde1999@gmail.com>
2024-05-09 18:10:04 +0530

    Added loop to TopNav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,28 +10,24 @@ export const metadata: Metadata = {
   description: "This is my portfolio",
 };
 
+const NavList = [
+  ["Home", "/"],
+  ["About", "/about"],
+  ["Education", "/education"],
+  ["Experience", "/experience"],
+  ["Projects", "/projects"],
+  ["Contact", "/contact"],
+];
+
 function TopNav() {
   return (
     <nav>
       <ul className="flex flex-wrap gap-4 pad-4 py-5 justify-center space-x-4">
-        <li className="font-bold px-3 py-2 text-slate-400 rounded-lg hover:bg-slate-100 hover:text-slate-900">
-          <Link href="/">Home</Link>
-        </li>
-        <li className="font-bold px-3 py-2 text-slate-400 rounded-lg hover:bg-slate-100 hover:text-slate-900">
-          <Link href="/about">About</Link>
-        </li>
-        <li className="font-bold px-3 py-2 text-slate-400 rounded-lg hover:bg-slate-100 hover:text-slate-900">
-          <Link href="/education">Education</Link>
-        </li>
-        <li className="font-bold px-3 py-2 text-slate-400 rounded-lg hover:bg-slate-100 hover:text-slate-900">
-          <Link href="/experience">Experience</Link>
-        </li>
-        <li className="font-bold px-3 py-2 text-slate-400 rounded-lg hover:bg-slate-100 hover:text-slate-900">
-          <Link href="/projects">Projects</Link>
-        </li>
-        <li className="font-bold px-3 py-2 text-slate-400 rounded-lg hover:bg-slate-100 hover:text-slate-900">
-          <Link href="/contact">Contact</Link>
-        </li>
+        {NavList.map(([name, href]) => (
+          <li className="font-bold px-3 py-2 text-slate-400 rounded-lg hover:bg-slate-100 hover:text-slate-900">
+            <Link href={href}>{name}</Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
