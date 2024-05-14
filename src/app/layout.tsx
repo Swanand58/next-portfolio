274e8ccf7d3b858aacb95841830baeae55c940fd
Swanand58 <swanandkhonde1999@gmail.com>
2024-05-14 09:06:39 +0530

    Added responsive styling for header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ const NavList = [
 function TopNav() {
   return (
     <nav>
-      <ul className="flex w-full gap-4 pad-4 py-5 justify-center space-x-4">
+      <ul className="flex w-full flex-wrap gap-4 pad-4 py-5 justify-center space-x-4">
         {NavList.map(([name, href]) => (
           <li className="font-bold px-3 py-2 text-slate-400 rounded-lg hover:bg-slate-100 hover:text-slate-900">
             <Link href={href}>{name}</Link>
@@ -34,9 +34,9 @@ function TopNav() {
 
 function Footer() {
   return (
-    <footer className="flex w-full py-4 border-t border-gray-200 items-center justify-between px-10">
-      <div className="flex-grow">
-        <ul className="flex space-x-4">
+    <footer className="items-center justify-between w-full py-4 border-t border-gray-200 px-4 sm:px-10">
+      <div className="flex flex-col sm:flex-row justify-between items-center text-center sm:text-left">
+        <ul className="flex justify-center flex-wrap gap-2 mb-4 sm:mb-0 items-center">
           {NavList.map(([name, href]) => (
             <li className="font-bold px-2 py-2 text-sm text-gray-600 hover:text-white ">
               <Link href={href}>{name}</Link>
@@ -44,9 +44,9 @@ function Footer() {
           ))}
         </ul>
       </div>
-      <p className="text-gray-600">
-        © 2024 Swanand Khonde. All Rights Reserved.
-      </p>
+      <div className="flex justify-center w-full text-gray-600 text-xs sm:text-sm items-center sm:w-auto sm:ml-auto">
+        <p className="">© 2024 Swanand Khonde. All Rights Reserved.</p>
+      </div>
     </footer>
   );
 }
