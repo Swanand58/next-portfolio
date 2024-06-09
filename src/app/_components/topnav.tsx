56af4ed761889b6eea04df3f03dce6a3bd61d61d
Swanand58 <swanandkhonde1999@gmail.com>
2024-06-09 11:13:27 +0530

    Added highlight font color to active nav tab

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -20,7 +20,7 @@ function TopNav() {
           <li
             key={name}
             className={`font-bold px-3 py-2 rounded-lg hover:bg-slate-100 hover:text-slate-900 ${
-              pathname === href ? "text-blue-200" : "text-slate-400"
+              pathname === href ? "text-white" : "text-slate-400"
             }`}
           >
             <Link href={href}>{name}</Link>
