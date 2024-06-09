3a71c556be13919eace6fe53765b8f303aeeb629
Swanand58 <swanandkhonde1999@gmail.com>
2024-06-09 20:41:25 +0530

    Added underlines

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -20,7 +20,9 @@ function TopNav() {
           <li
             key={name}
             className={`font-bold px-3 py-2 rounded-lg hover:bg-slate-100 hover:text-slate-900 ${
-              pathname === href ? "text-white" : "text-slate-400"
+              pathname === href
+                ? "text-white underline-offset-4 underline"
+                : "text-slate-400"
             }`}
           >
             <Link href={href}>{name}</Link>
