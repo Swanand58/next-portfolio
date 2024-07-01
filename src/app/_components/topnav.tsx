61f049ca9e7c9fcde4c6ddd5010876abad319516
Swanand58 <swanandkhonde1999@gmail.com>
2024-06-30 22:17:11 -0500

    Updated Resume

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -19,7 +19,7 @@ function TopNav() {
         {NavList.map(([name, href]) => (
           <li
             key={name}
-            className={`font-bold px-3 py-2 rounded-xl hover:text-white hover:bg-slate-600 ${
+            className={`font-bold px-3 py-2 rounded-lg hover:text-white hover:bg-slate-600 ${
               pathname === href
                 ? "text-white underline-offset-4 underline decoration-gray-400"
                 : "text-slate-400"
