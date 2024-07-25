8034eb1b4364bee7ca1aad9cac3efcf394ab864a
Swanand58 <swanandkhonde1999@gmail.com>
2024-07-25 18:09:20 -0500

    Removed images from project cards

--- data/projects.ts
+++ data/projects.ts
@@ -9,7 +9,6 @@ import { SiNvidia } from "react-icons/si";
 import { FaGolang } from "react-icons/fa6";
 
 interface Project {
-  image: string;
   title: string;
   description: string;
   Icon: IconType;
@@ -18,7 +17,6 @@ interface Project {
 
 const projects: Project[] = [
   {
-    image: "/projects/othello.png",
     title: "Othello Reversi Game using Cilk Plus",
     description:
       "A parallelized version of the classic strategy game Othello (also known as Reversi), implemented in Cilk Plus. Implements the negamax algorithm for AI decision-making.",
@@ -26,7 +24,6 @@ const projects: Project[] = [
     link: "https://github.com/Swanand58/Othello_CILKPLUS",
   },
   {
-    image: "/projects/portfolio.jpeg",
     title: "Next.js Portfolio",
     description:
       "This Website. My portfolio website built with Next.js and Tailwind CSS.",
@@ -34,7 +31,6 @@ const projects: Project[] = [
     link: "https://github.com/Swanand58/next-portfolio",
   },
   {
-    image: "/projects/social-media-fe.jpg",
     title: "Social Media App Frontend",
     description:
       "A fully scalable social media platform in MERN stack. Frontend written in React.js",
@@ -42,7 +38,6 @@ const projects: Project[] = [
     link: "https://github.com/Swanand58/social-media-app-frontend-rice",
   },
   {
-    image: "/projects/cuda.jpg",
     title: "Data Parallel Hole Compaction using CUDA",
     description:
       "Implementation of parallel hole compaction using CUDA, a parallel computing platform and application programming interface model created by NVIDIA",
@@ -50,7 +45,6 @@ const projects: Project[] = [
     link: "https://github.com/Swanand58/parallel-hole-compaction-cuda",
   },
   {
-    image: "/projects/lu.jpg",
     title: "LU Decomposition using OpenMP",
     description:
       "A parallel implementations of LU decomposition that use Gaussian elimination to factor a dense N x N matrix into an upper-triangular one and a lower-triangular one.",
@@ -58,7 +52,6 @@ const projects: Project[] = [
     link: "https://github.com/Swanand58/LU_Decomposition_OPENMP",
   },
   {
-    image: "/projects/spotify-be.png",
     title: "Spotify Music Party Backend",
     description:
       "A work in progess project where a group of music enthusiasts are building a music party app using spotify developer APIs",
@@ -66,7 +59,6 @@ const projects: Project[] = [
     link: "https://github.com/Swanand58/spotify-music-party-backend",
   },
   {
-    image: "/projects/spotify-fe.jpg",
     title: "Spotify Music Party Frontend",
     description:
       "A work in progess project where a group of music enthusiasts are building a music party app using spotify developer APIs",
@@ -74,7 +66,6 @@ const projects: Project[] = [
     link: "https://github.com/Swanand58/spotify-music-party-frontend",
   },
   {
-    image: "/projects/event.jpg",
     title: "OES Rice Event Management App",
     description:
       "A work in progress project for a fully scalable event management web application for the Ralph O'connor Building of Engineering Sciences at Rice University.",
@@ -82,7 +73,6 @@ const projects: Project[] = [
     link: "https://github.com/Swanand58/OES-Rice-EventManagement",
   },
   {
-    image: "/projects/dp.webp",
     title: "Ball World Design Patterns",
     description:
       "A web application that draws random-sized balls at random locations on a canvas which has different moving behavior. The application follows the MVC programming paradigm and utilizes various design patterns to structure the code",
@@ -90,7 +80,6 @@ const projects: Project[] = [
     link: "https://github.com/Swanand58/ball-world-design-pattern-exercise",
   },
   {
-    image: "/projects/social-media-be.png",
     title: "Social Media App Backend",
     description:
       "A fully scalable social media platform in MERN stack. Backend written in Node.js (Express.js) and MongoDB.",
@@ -98,14 +87,12 @@ const projects: Project[] = [
     link: "https://github.com/Swanand58/social-media-app-backend-rice",
   },
   {
-    image: "/projects/2-cars.jpg",
     title: "2-Cars Javascript Game",
     description: "Interactive 2-cars game built with JavaScript.",
     Icon: SiJavascript,
     link: "https://github.com/Swanand58/2-cars-javascript",
   },
   {
-    image: "/projects/online-auction.png",
     title: "CodeFury 2021 Hackathon HSBC Online Auction",
     description:
       "Built a Full Stack online auction platform in HSBC Hackathon CodeFury 2021",
@@ -113,7 +100,6 @@ const projects: Project[] = [
     link: "https://github.com/Swanand58/code-fury-2021",
   },
   {
-    image: "/projects/document-classification.png",
     title: "Document Classification",
     description:
       "'Document Classification using visual parameters', a deep learning project aimed at streamlining document categorization (letters, emails, resumes, invoices, etc.) for user convenience",
@@ -121,7 +107,6 @@ const projects: Project[] = [
     link: "https://github.com/Swanand58/Document_Classification",
   },
   {
-    image: "/projects/snake.png",
     title: "Classic Snake Game",
     description:
       "A snake game built with Pygames Python library. The game includes features such as a growing snake, food spawning, special food spawning, score tracking, and many more.",
@@ -129,7 +114,6 @@ const projects: Project[] = [
     link: "https://github.com/Swanand58/classic-snake-game",
   },
   {
-    image: "/projects/sfm.png",
     title: "Structure from Motion",
     description:
       "Structure from Motion (SfM) algorithm optimized for architectural exteriors, generating 3D models.",
@@ -137,7 +121,6 @@ const projects: Project[] = [
     link: "https://github.com/Swanand58/structure-from-motion",
   },
   {
-    image: "/projects/cd.png",
     title: "Compiler Design",
     description:
       "Compiler Design Course Assignments, Course from Vishwakarma Institute of Technology - Pune",
@@ -145,7 +128,6 @@ const projects: Project[] = [
     link: "https://github.com/Swanand58/CompilerDesign",
   },
   {
-    image: "/projects/sudoku.jpeg",
     title: "Sudoku Game",
     description:
       "A Sudoku game built with python. GUI for Human Playable game and used Backtracking Algorithm for ai playing the game.",
--- src/app/_components/projectcards.tsx
+++ src/app/_components/projectcards.tsx
@@ -4,7 +4,6 @@ import { GrView } from "react-icons/gr";
 import Image from "next/image";
 
 interface ProjectCardProps {
-  image: string;
   title: string;
   description: string;
   Icon: IconType;
@@ -12,42 +11,28 @@ interface ProjectCardProps {
 }
 
 const ProjectCards: React.FC<ProjectCardProps> = ({
-  image,
   title,
   description,
   Icon,
   link,
 }) => {
   return (
-    <div
-      className="relative bg-[rgb(51,67,101)] dark:bg-slate-500 text-white rounded-lg shadow-md m-1 sm:m-2 transition duration-300 ease-in-out hover:shadow-xl hover:bg-[rgb(41,55,91)] dark:hover:bg-slate-800 flex flex-col justify-between h-full cursor-pointer overflow-hidden"
-      onClick={() => window.open(link, "_blank")}
-    >
-      <div className="relative w-full h-32 sm:h-48 p-2">
-        <div className="relative w-full h-32 sm:h-48">
-          <Image
-            src={image}
-            alt={title}
-            layout="fill"
-            objectFit="cover"
-            className="rounded-lg"
-          />
+    <div className="bg-[rgb(51,67,101)] dark:bg-slate-500 text-white p-2 sm:p-4 rounded-lg shadow-md m-1 sm:m-2 transition duration-300 ease-in-out hover:shadow-xl hover:bg-[rgb(41,55,91)] dark:hover:bg-slate-800 flex flex-col justify-between h-full">
+      <div>
+        <div className="flex items-center gap-2 sm:gap-3 mb-1 sm:mb-2">
+          <Icon className="text-xl w-8 sm:w-10 h-8 sm:h-10" />
+          <h3 className="text-base sm:text-lg font-bold">{title}</h3>
         </div>
+        <p className="mb-2 mt-6 sm:mb-4 flex-1 text-sm sm:text-base">
+          {description}
+        </p>
       </div>
-      <div className="p-2 sm:p-4 flex-1 flex flex-col justify-between card-content transition filter duration-300 ease-in-out">
-        <div>
-          <div className="flex items-center gap-2 sm:gap-3 mb-1 sm:mb-2 mt-2 sm:mt-1">
-            <Icon className="text-xl w-8 sm:w-10 h-8 sm:h-10" />
-            <h3 className="text-base sm:text-lg font-bold">{title}</h3>
-          </div>
-          <p className="mt-4 sm:mb-4 flex-1 text-sm sm:text-base">
-            {description}
-          </p>
-        </div>
-      </div>
-      <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out bg-black bg-opacity-50">
-        <GrView className="text-4xl text-white hover:text-[rgb(51,67,101)]" />
-      </div>
+      <a
+        href={link}
+        className="hover:text-blue-300 p-1 sm:p-2 rounded font-semibold transition-colors text-left mt-1 sm:mt-2"
+      >
+        View Project
+      </a>
     </div>
   );
 };
