8d2d18395831b43d4f29b81dcf7bac5883d099d7
Swanand58 <swanandkhonde1999@gmail.com>
2024-09-08 11:31:01 -0500

    Improved SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Footer from "./_components/footer";
 import TopNav from "./_components/topnav";
 import ThemeToggle from "../app/_components/ThemeToggle";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,43 @@ export default function RootLayout({
   return (
     <html lang="en">
       <CSPostHogProvider>
+        <Head>
+          <title>Swanand Sanjay Khonde</title>
+          <meta
+            name="description"
+            content="My personal website showcasing my projects and experience."
+          />
+          <meta
+            name="keywords"
+            content="Swanand Khonde, software engineer, full-stack developer, portfolio, swanand sanjay khonde, machine learning engineer"
+          />
+          <link
+            rel="canonical"
+            href="https://swanand-khonde-portfolio.vercel.app/"
+          />
+
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify({
+                "@context": "https://schema.org",
+                "@type": "Person",
+                name: "Swanand Sanjay Khonde",
+                url: "https://swanand-khonde-portfolio.vercel.app/",
+                jobTitle: "Software Engineer",
+                image: "https://swanand-khonde-portfolio.vercel.app/logo.jpg",
+                sameAs: [
+                  "https://www.linkedin.com/in/swanandkhonde/",
+                  "https://github.com/Swanand58",
+                ],
+                worksFor: {
+                  "@type": "Organization",
+                  name: "HSBC",
+                },
+              }),
+            }}
+          />
+        </Head>
         <body
           className={`${inter.className} bg-gray-200 dark:bg-black text-black dark:text-white`}
         >
